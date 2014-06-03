4e6735ae283503a3cf13d43d4209e3e4ed072a3d
XKC <xkc2000115@qq.com>
2014-06-03 13:26:11 +0800

    3

---
+++ CodeBlocks/Console application/main (used windows.h)/Sp__test/main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+#include <windows.h>
+
+void ING(void);
+int main()
+{
+    int i,a,b,L,I,C,n = 0;
+    ING();
+    printf("number:");
+    scanf("%d",&n);
+    printf("From:");
+    scanf("%d",&a);
+    printf("  to:");
+    scanf("%d",&b);
+    printf("\n move in 3s");
+    Sleep(3000);
+    for(i=0;i<=n;i++)
+    {
+        L=rand()%(b-a+1)+a;
+        for(I=0;I<L;I++)
+        {
+            C=rand()%26+65;
+            keybd_event(C,0,0,0);
+            Sleep(1);
+            keybd_event(C,0,KEYEVENTF_KEYUP,0);
+            Sleep(1);
+        }
+        keybd_event(32,0,0,0);
+        Sleep(1);
+        keybd_event(32,0,KEYEVENTF_KEYUP,0);
+        Sleep(1);
+        keybd_event(13,0,0,0);
+        Sleep(1);
+        keybd_event(13,0,KEYEVENTF_KEYUP,0);
+        Sleep(1);
+        keybd_event(0x11,0,0,0);
+        Sleep(1);
+        keybd_event(0x0D,0,0,0);
+        Sleep(1);
+        keybd_event(0x0D,0,KEYEVENTF_KEYUP,0);
+        Sleep(1);
+        keybd_event(0x11,0,KEYEVENTF_KEYUP,0);
+        Sleep(5);
+        printf("%d\n",i);
+    }
+    return 0;
+}
+
+void ING(void)
+{
+    srand((unsigned) time(NULL));
+}
